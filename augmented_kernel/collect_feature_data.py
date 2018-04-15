23b70106b67b9799b19942cbffee1337f55035b7
sbhadsavle <sarang.bhadsavle@gmail.com>
2018-04-14 19:38:05 -0500

    Encapsulated data collection script into functions

--- augmented_kernel/collect_feature_data.py
+++ augmented_kernel/collect_feature_data.py
@@ -4,28 +4,40 @@ import re, string
 import perspective
 import azure
 
-print("Reading input csv...")
-train = pd.read_csv('../../input/train500.csv')
+def azure_text_analysis(train_df):
+	# print(train_df["comment_text"])
+	result_df = azure.analyze_text(train_df["comment_text"])
+	# print(result_df)
+	return result_df
 
-print("Azure text analysis...")
-# print(train["comment_text"])
-azure_result_df = azure.analyze_text(train["comment_text"])
-# print(result_df)
+def perspective_text_analysis(train_df):
+	perspective_vals = []
+	for i,comment in enumerate(train_df["comment_text"]):
+		print("[Perspective API] Analyzing comment ", i)
+		if (len(comment) < 3000):
+			perspective_vals.append(perspective.get_perspective_toxicity(comment))
+		else:
+			perspective_vals.append(0.5) # default to "unknown" toxicity if length is too long
 
-train = train.join(azure_result_df)
-# print(train.head())
+	result_df = pd.DataFrame({'perspective_toxicities': perspective_vals})
+	return result_df
 
-print("Perspective text analysis...")
-perspective_vals = []
-for comment in train["comment_text"]:
-	if (len(comment) < 3000):
-		perspective_vals.append(perspective.get_perspective_toxicity(comment))
-	else:
-		perspective_vals.append(0.5) # default to "unknown" toxicity if length is too long
+def main():
+	print("Reading input csv...")
+	train = pd.read_csv('../../input/train500.csv')
 
-persp_df = pd.DataFrame({'perspective_toxicities': perspective_vals})
-train = train.join(persp_df)
+	print("Azure text analysis...")
+	azure_df = azure_text_analysis(train)
+	train = train.join(azure_df)
+	# print(train.head())
 
-print("Writing csv...")
-train.to_csv('train_augmented.csv', index=False)
-print("Done.")
+	print("Perspective text analysis...")
+	persp_df = perspective_text_analysis(train)
+	train = train.join(persp_df)
+
+	print("Writing csv...")
+	train.to_csv('train_augmented.csv', index=False)
+	print("Done.")
+
+if __name__ == '__main__':
+	main()
